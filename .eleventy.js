066ff95cc7b606615152d4c9d24cfd8cd105f811
Kim Paulissen <kim.paulissen@kuleuven.be>
2020-09-13 00:03:19 +0200

    adjust structure and layout

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,12 @@
-module.exports = {
-  dir: {
-    input: "src",
-    output: "dist",
-  },
+module.exports = function (eleventyConfig) {
+  // Copy `src/img/` to `dist/img`
+  eleventyConfig.addPassthroughCopy("img");
+
+  // You can return your Config object (optional).
+  return {
+    dir: {
+      input: "src",
+      output: "dist",
+    },
+  };
 };
--- src/index.md
+++ src/index.md
@@ -1,6 +1,7 @@
 ---
 layout: layouts/base.njk
 title: spereverde docs
+tag: ['home']
 ---
 
 This site will contain a random snippets of documentation
